b1b9f9952a3a4c741806e912a39017f5e2f4e9ab
Rachel <rachel.fizel@gmail.com>
2021-01-18 14:45:57 -0600

    read CSV file and print the file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,18 @@
-main.py
+# module to create file paths across operating systems
+import os
+# module for reading CSV files
+import csv
+
+csvpath = os.path.join("Resources", "budget_data.csv")
+
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvreader)
+
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        print(row)
+        
