c8471f062495f0c3ba76a41c1e579331b9759a62
Rachel <rachel.fizel@gmail.com>
2021-01-19 16:46:13 -0600

    Added the greatest decrease.  Also fixed greatest increase to be the difference between the numbers.

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,19 +16,29 @@ with open(csvpath) as csvfile:
     count = 0
     total = 0
     greatest_increase_in_profit = 0
+    greatest_decrease_in_profit = 0
+    last_profit_loss = 0
 
     for row in csvreader:
 
         input_date = row[0]
         input_profit_loss = int(row[1])
+        difference_profit_loss = int(row[1]) - last_profit_loss
 
         count = count + 1
 
         total = total + input_profit_loss
 
-        if (input_profit_loss > greatest_increase_in_profit):
-            greatest_increase_in_profit = input_profit_loss
+        if (difference_profit_loss > greatest_increase_in_profit):
+            greatest_increase_in_profit = difference_profit_loss
             greatest_increase_in_profit_month = input_date
+
+        if (difference_profit_loss < greatest_decrease_in_profit):
+            greatest_decrease_in_profit = difference_profit_loss
+            greatest_decrease_in_profit_month = input_date
+
+        last_profit_loss = input_profit_loss
+
 
 
 
@@ -41,7 +51,7 @@ with open(csvpath) as csvfile:
     print("Total: $" + str(total))
 
     print("Greatest Increase in Profits: " + str(greatest_increase_in_profit_month) + " (" + str(greatest_increase_in_profit) + ")")
-    
+    print("Greatest Decrease in Profits: " + str(greatest_decrease_in_profit_month) + " (" + str(greatest_decrease_in_profit) + ")")
 
 
         
