d353b0b87638141eafd5a8316042d5d97cf21571
Rachel <rachel.fizel@gmail.com>
2021-01-21 08:37:35 -0600

    open election_data.csv and print Hello World

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,17 @@
-main.py
+# module to create file paths across operating systems
+import os
+# module for reading CSV files
+import csv
+
+csvpath = os.path.join("Resources", "election_data.csv")
+
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvreader)
+
+
+
+print ("Hello World")
+    
