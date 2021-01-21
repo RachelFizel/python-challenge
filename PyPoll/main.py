4508aaa66ab47f8a1435e89b06cebff09e343d0f
Rachel <rachel.fizel@gmail.com>
2021-01-21 08:56:08 -0600

    added total count of votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,10 @@ import csv
 
 csvpath = os.path.join("Resources", "election_data.csv")
 
+
+total_count_of_votes = 0
+
+
 # Open the CSV
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -12,8 +16,22 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
 
+    #loop through each row in the csv file
+    for row in csvreader:
+
+        input_voter_id = row[0]
+        input_county = row[1]
+        input_candidate = row[2]
+
+        #count the number of votes cast
+        total_count_of_votes += 1
+
+   
+
 
 print (" ")
 print ("Election Results")
 print ("------------------------")
-    
+
+print ("Total Votes: " + str(total_count_of_votes))
+print ("------------------------")
